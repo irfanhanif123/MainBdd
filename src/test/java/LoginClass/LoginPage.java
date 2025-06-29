0018f2909d3b9253ab961da4c2b7655cd067b12b
LENOVO <mirfanhanif@hotmail.com>
2025-06-29 09:22:57 -0700

    new change

--- src/test/java/LoginClass/LoginPage.java
+++ src/test/java/LoginClass/LoginPage.java
@@ -10,6 +10,7 @@ public class LoginPage {
     private By Btn_Login = By.xpath("//*[@id=\"login-button\"]");
     private By Product_sign = By.xpath("//*[@id=\"header_container\"]/div[2]/span");
 
+
     public LoginPage(WebDriver driver) {
 
 this.driver=driver;
@@ -38,6 +39,7 @@ public void loginValidUser(String username,String password){
         driver.findElement(txt_username).sendKeys(username);
 driver.findElement(text_Password).sendKeys(password);
     driver.findElement(Btn_Login).click();
+    System.out.println("jjkhkjd");
     }
 
 
